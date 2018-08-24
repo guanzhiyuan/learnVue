63c7a6507d1f52e73ea2e875c2ba0aa1b50a0095
guanzhiyuan <756027005@qq.com>
2018-08-24 14:55:30 +0800

    add

--- myVue/src/components/content/system/userAdd.vue
+++ myVue/src/components/content/system/userAdd.vue
@@ -5,15 +5,82 @@
 			<el-breadcrumb-item :to="{ path: '/sysUser' }">用户管理</el-breadcrumb-item>
 			<el-breadcrumb-item>新增用户</el-breadcrumb-item>
 		</el-breadcrumb>
+		<div id="userAddContent">
+			<el-form ref="form" :model="form" label-width="100px" label-position='right' :rules="rules" >
+				<el-form-item label="用户名:" prop="userName">
+					<el-input v-model="form.userName" placeholder="请输入用户名"></el-input>
+				</el-form-item>
+				<el-form-item label="身份证:" prop="userIdNum">
+					<el-input v-model="form.userIdNum" placeholder="请输入身份证"></el-input>
+				</el-form-item>
+				<el-form-item label="联系方式:" prop="userTel">
+					<el-input v-model="form.userTel" placeholder="请输入联系方式"></el-input>
+				</el-form-item>
+				<el-form-item label="性别">
+					<el-radio-group v-model="form.sex">
+						<el-radio label="1">男</el-radio>
+						<el-radio label="0">女</el-radio>
+					</el-radio-group>
+				</el-form-item>
+				<el-form-item>
+					<el-button type="primary" @click="add('form')">立即创建</el-button>
+					<el-button @click="reset('form')">重置</el-button>
+				</el-form-item>
+			</el-form>
+		</div>
 	</div>
 </template>
 
 <script>
 	export default {
-		name: "userAdd"
+		name: "userAdd",
+		data: function() {
+			return {
+				form: {
+					userName: "",
+					userIdNum: "",
+					userTel: "",
+					sex: "1"
+				},
+				rules: {
+					userName: [{
+						required: true,
+						message: '请输入用户名',
+						trigger: 'blur'
+					}],
+					userIdNum: [{
+						required: true,
+						message: '请输入身份证号码',
+						trigger: 'blur'
+					}],
+					userTel: [{
+						required: true,
+						message: '请输入联系方式',
+						trigger: 'blur'
+					}]
+				}
+			}
+		},
+		methods: {
+			reset(form) {
+				this.$refs[form].resetFields();
+			},
+			add(form) {
+				this.$refs[form].validate((valid) => {
+					if(valid) {
+						alert('submit!');
+					} else {
+						return false;
+					}
+				});
+			}
+		}
 	}
 </script>
 
 <style>
-
+	#userAddContent {
+		margin: 20px 0;
+		width: 500px;
+	}
 </style>
--- myVue/src/components/head/header.vue
+++ myVue/src/components/head/header.vue
@@ -10,6 +10,8 @@
 </template>
 
 <script>
+	import '../../../static/css/header.less' 
+	
 	export default {
 		name: 'myHeader',
 		data() {
@@ -21,36 +23,5 @@
 </script>
 
 <style>
-	#myHeader {
-		height: 60px;
-		position: fixed;
-		width: 100%;
-		top: 0;
-		left: 0;
-		text-align: left;
-		padding: 0 90px;
-		box-sizing: border-box;
-		z-index: 99999;
-		background: #fff;
-	}
 	
-	#myHeader img#logo {
-		vertical-align: middle;
-		width: 80px;
-		height: 50px;
-		display: inline-block;
-	}
-	
-	#myHeader h3 {
-		display: inline-block;
-	}
-	
-	#myHeader ul {
-		list-style: none;
-		float: right;
-	}
-	
-	#myHeader ul li {
-		display: inline-block;
-	}
 </style>
